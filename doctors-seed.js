491cefc598961fa0a32e2b4ea4a57fdf8e93cd25
aidarbuy <aidarbuy@gmail.com>
2015-03-31 12:25:45 -0400

    inde.html

--- doctors-seed.js
+++ doctors-seed.js
@@ -9,7 +9,8 @@ module.exports = [
 		education: "UVM College of Medicine, Johns Hopkins School of Medicine",
 		imageSmall: "images/doctors/roshelle-440x440.jpg",
 		imageLarge: "images/doctors/roshelle-820x465.jpg",
-		description: "I graduated from University of Vermont College of Medicine in 2005, then I finished my residency training in emergency medicine from Johns Hopkins University School of Medicine in 2008. I worked as an emergency room physician in Frederick Memorial Hospital until 2013. Currently, in addition to working in Leesburg Urgent Care, I also work as a special operations emergency room physician for Team Health.",
+		description: "I graduated from University of Vermont College of Medicine in 2005, then I finished my residency training in emergency medicine from Johns Hopkins University School of Medicine in 2008. I worked as an emergency room physician in Frederick Memorial Hospital until 2013. Currently, in addition to working in Leesburg Urgent Care, I also work as a special operations emergency room physician for Team Health. I provide urgent care and primary care services.",
+
 		email: "rbeckwith@leesburguc.com"
 	},
 	{
@@ -22,7 +23,7 @@ module.exports = [
 		education: "University of Texas School of Medicine",
 		imageSmall: "images/doctors/matthew-440x440.jpg",
 		imageLarge: "images/doctors/matthew-820x465.jpg",
-		description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+		description: "I graduated from University of Texas School of Medicine in 1995. I finished my residency training in family medicine from University of Florida n 1998. I have been working as a family doctor in Robinwood Medical Center since then. I provide primary care services.",
 		email: "mbeckwith@leesburguc.com"
 	},
 	{
